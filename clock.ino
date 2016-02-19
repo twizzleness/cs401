ec5c079e4240857ef0f95066f628e2ae08be1173
twizzleness <tylerforaie@gmail.com>
2016-02-19 00:00:49 -0600

    Added files via upload
    Keeps track of the time

---
+++ clock.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+
+int clockInt = 0;            // digital pin 2 is now interrupt 0
+int masterClock = 0;         // counts rising edge clock signals
+int seconds = 0;             // variable
+int minutes = 0;             // variable
+int ledPin = 13;
+int ledState = LOW;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  attachInterrupt(clockInt, clockCounter, CHANGE);
+  //  clockInt is our interrupt, clockCounter function is called when
+  //  invoked on a RISING clock edge
+  analogReference(DEFAULT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  analogWrite(0, 127);   // this starts our PWM 'clock' with a 50% duty cycle
+}
+
+
+void clockCounter()      // called by interrupt
+{
+  Serial.println("Look im in an interupt");
+  masterClock ++;        // with each clock rise add 1 to masterclock count
+  if(masterClock == 489) // 490Hz reached     
+  {                         
+    seconds ++;          // after one 490Hz cycle add 1 second ;)
+    masterClock = 0;     // Reset after 1 second is reached
+    tone(13, 100, 500);  // using tone to pulse LED without delay call ;)
+  }
+  return;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println("clocks are dumb");
+ 
+  if(seconds == 60)      // NOW GETTING IN TO REAL TIME KEEPING
+  {
+    minutes ++;          // increment minutes by 1
+    seconds = 0;         // reset the seconds variable
+    Serial.print("Minutes = ");
+    Serial.println(minutes);
+  }
+}
